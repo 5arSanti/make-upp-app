0ff88341145c8ecce266dab33300f8a43b1b1fa0
5arSanti <santiari05@hotmail.com>
2025-10-13 17:36:27 -0500

    Update capacitor.config.ts to change appId and appName for better branding alignment.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'make-upp-app',
+  appId: 'co.makeupp.app',
+  appName: 'make-upp',
   webDir: 'dist'
 };
 
